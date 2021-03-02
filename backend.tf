102f43a61fa7d5edd8cafa7a0b6f35860b084510
tonysonaike <rgtunes.com>
2021-03-01 21:06:57 -0600

    my new cicd jenkins-terraform

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "jjtech-ci-cd-terraform"
     key    = "state/jjtech.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
     profile = "default"
   }
 }
